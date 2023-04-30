4c636ec9105bd27e7a776b719ad2ba7ec74107c0
Tetiana Nivchyk <tata.02od@gmail.com>
2023-04-30 21:58:38 +0300

    fixed

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,40 +1,40 @@
-import { format } from 'date-fns';
 import PropTypes from 'prop-types';
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import { formatDate } from 'utiles/getDate';
 
+export const CryptoHistory = ({ list }) => {
+  return (
+    <BaseTable>
+      <THead>
+        <tr>
+          <Th>№</Th>
+          <Th>PRICE</Th>
+          <Th>AMOUNT</Th>
+          <Th>DATE</Th>
+        </tr>
+      </THead>
 
-export const CryptoHistory = ({list}) => {
-  return <BaseTable>
-  <THead>
-    <tr>
-      <Th>№</Th>
-      <Th>PRICE</Th>
-      <Th>AMOUNT</Th>
-      <Th>DATE</Th>
-    </tr>
-  </THead>
-
-    <tbody>
-      {list.map(({id,price,amount,date},index) => <Tr key={id}>
-      <Td>{index+1}</Td>
-      <Td>{price}</Td>
-      <Td>{amount}</Td>
-      <Td>{format(new Date(date), 'yyyy-MM-dd, h:mm a')}</Td>
-    </Tr>)}
-   
-   
-  </tbody>
-</BaseTable>;
+      <tbody>
+        {list.map(({ id, price, amount, date }, index) => (
+          <Tr key={id}>
+            <Td>{index + 1}</Td>
+            <Td>{price}</Td>
+            <Td>{amount}</Td>
+            <Td>{formatDate(date)}</Td>
+          </Tr>
+        ))}
+      </tbody>
+    </BaseTable>
+  );
 };
-
 
 CryptoHistory.propTypes = {
   list: PropTypes.arrayOf(
-  PropTypes.shape({
-  id: PropTypes.number.isRequired,
-  price: PropTypes.number.isRequired,
-  amount: PropTypes.number.isRequired,
-  date: PropTypes.string.isRequired,
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+      amount: PropTypes.number.isRequired,
+      date: PropTypes.string.isRequired,
     }),
   ),
 };
--- src/utiles/getDate.js
+++ src/utiles/getDate.js
@@ -1,5 +1,9 @@
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, format } from 'date-fns';
 
 export function getDate(date) {
   return formatDistanceToNow(new Date(date), { addSuffix: true });
+}
+
+export function formatDate(date) {
+  return format(new Date(date), 'yyyy-MM-dd, h:mm a');
 }
