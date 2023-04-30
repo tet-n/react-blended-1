787f103d09bc127a71307506e810d12c37d613ed
VVladyslav <VVVasylyshyn@gmail.com>
2023-04-30 16:06:20 +0300

    task-2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,6 +8,9 @@ import {
   CryptoHistory,
 } from 'components';
 import article from 'data/article.json';
+import data from 'data/data.json';
+
+
 
 export const App = () => {
   return (
@@ -21,7 +24,7 @@ export const App = () => {
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 2
         </Heading>
-        <Statistics />
+        <Statistics title="Main Statistics" stats={data} />
 
         <Heading marginTop="50px" marginBottom="50px" textAlign="center">
           Task 3
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,40 @@
+import PropTypes from 'prop-types';
 import { StatisticItem } from 'components';
 import { StatisticsList, StatisticTitle } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
 import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
 
-export const Statistics = () => {
+const icons = [
+  <FaRegThumbsUp />,
+  <MdPeople />,
+  <MdOutlineProductionQuantityLimits />,
+  <GiTreeDoor />,
+];
+
+export const Statistics = ({ title, stats }) => {
   return (
     <>
-      <div>StatisticsList</div>
+      {title && <StatisticTitle>{title}</StatisticTitle>}
+      <StatisticsList>
+        {stats.map((stat, index) => (
+          <StatisticItem
+            title={stat.title}
+            total={stat.total}
+            key={stat.id}
+            icon={icons[index]}
+          />
+        ))}
+      </StatisticsList>
     </>
   );
+};
+
+Statistics.propTypes = {
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  title: PropTypes.string,
 };
