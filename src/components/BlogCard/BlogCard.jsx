e066280adbe75661d4fea227879470a7d282d871
Tetiana Nivchyk <tata.02od@gmail.com>
2023-04-30 15:23:58 +0300

    finished task 1

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,61 @@
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import PropTypes from 'prop-types';
+
+import {
+  Card,
+  CardHeader,
+  CardPoster,
+  CardBody,
+  Tag,
+  CardTitle,
+  CardText,
+  CardFooter,
+  UserBox,
+  Avatar,
+  UserInfo,
+  UserName,
+  Date,
+} from './BlogCard.styled';
+
+import { getDate } from 'utiles/getDate';
+
+export const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  name,
+  avatar,
+  postedAt,
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={tag} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={name} />
+          <UserInfo>
+            <UserName>{name}</UserName>
+            <Date>{getDate(postedAt)}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
+};
+
+BlogCard.propTypes = {
+  poster: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  postedAt: PropTypes.string.isRequired,
 };
